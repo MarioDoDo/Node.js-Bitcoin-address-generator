b863abc9de69201df7fc31aaf49b634c5847195f
MarioDoDo <75090058+MarioDoDo@users.noreply.github.com>
2021-04-19 20:54:10 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const Bitcoin = require("bitcoin-address-generator");
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = 4000
+
+let address;
+function gen(){
+
+Bitcoin.createWalletAddress((response) => {
+  address = response;
+});
+
+}
+
+app.use(express.static('views'))
+  app.set('view engine', 'ejs');
+  app.get('/', (req, res) => {
+    res.render('index')
+  })
+  
+  app.use(bodyParser.urlencoded({ extended: true })); 
+  app.post("/address", (req, res) => {
+    gen();
+    res.render('address', {address:address})
+  })
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
